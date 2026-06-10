d70c2130d40ab6644434a4c88d22b3fdf784a51d
KikhGr <kyriakh.d.grigoriadi@gmail.com>
2026-06-11 02:49:29 +0300

    Update MovieCards.jsx

--- HCI Group Assignment/src/components/MovieCards.jsx
+++ HCI Group Assignment/src/components/MovieCards.jsx
@@ -1,11 +1,11 @@
-export default function movie ({movie}) {
-
-    return (
-        <div className="movie">
-            <div><img src={movie.image}/></div>
-            <div>{movie.title}</div>
-            <div>{movie.description}</div>
-        </div>
-    );
-    
+export default function movie({ movie }) {
+  return (
+    <div className="movie">
+      <div>
+        <img src={movie.image} className="movieimg" />
+      </div>
+      <div className="fsize">{movie.title}</div>
+      <div>{movie.description}</div>
+    </div>
+  );
 }
