c1f95db886de0a63bcb2ad7843da4c1421a61de6
KikhGr <kyriakh.d.grigoriadi@gmail.com>
2026-06-11 02:50:54 +0300

    Update App.jsx

--- HCI Group Assignment/src/App.jsx
+++ HCI Group Assignment/src/App.jsx
@@ -8,32 +8,32 @@ export default function App () {
     return (
       <>
         <header className="headerspec">
-          <img className="hederimg" src={logo} />
-          <div className="headerbutton flex">
-            <div className="logbtnmargin">
-              <Theme />
-            </div>
-            <div className="loginbtn">
-              <UserLogState className="fonts" className="headerbutton" />
-            </div>
+        <img className="hederimg" src={logo} />
+        <div className="headerbutton flex">
+          <div className="logbtnmargin">
+            <Theme />
           </div>
-        </header>
-        <div>
-          <div>
-            <select>
-              <option selected>All</option>
-              <option>Action</option>
-              <option>Comedy</option>
-              <option>Horor</option>
-              <option>Adventure</option>
-              <option>Sci-Fi</option>
-            </select>
-          </div>
-          <div>
-            <Movies movies={movies} />
-            <Movies movies={movies} />
+          <div className="loginbtn">
+            <UserLogState className="fonts" className="headerbutton" />
           </div>
         </div>
+      </header>
+      <div className="containerstyle">
+        <div>
+          <select className="categorybuton fonts">
+            <option selected>Category</option>
+            <option>Action</option>
+            <option>Comedy</option>
+            <option>Horor</option>
+            <option>Adventure</option>
+            <option>Sci-Fi</option>
+          </select>
+        </div>
+        <div className="moviemargin">
+          <Movies movies={movies} />
+          <Movies movies={movies} />
+        </div>
+      </div>
       </>
     );
 }
