41a4f9ea76f24d3f719b7eda64e9019e94f98c8b
BesnikNu <besniknouredini@gmail.com>
2026-06-11 01:48:55 +0300

    Add files via upload

--- HCI Group Assignment/src/App.jsx
+++ HCI Group Assignment/src/App.jsx
@@ -1,19 +1,28 @@
 import Theme from "./components/Theme";
 import UserLogState from "./components/UserLogState.jsx";
 import logo from "./assets/logoLM.png";
+import Movies from "./components/Movies.jsx";
+import movies from "../src/movies.js";
 
 export default function App () {
     return (
-       <header className="headerspec">
-        <img className="hederimg" src={logo} />
-        <div className="headerbutton flex">
-          <div className="logbtnmargin">
-            <Theme />
+      <>
+        <header className="headerspec">
+          <img className="hederimg" src={logo} />
+          <div className="headerbutton flex">
+            <div className="logbtnmargin">
+              <Theme />
+            </div>
+            <div className="loginbtn">
+              <UserLogState className="fonts" className="headerbutton" />
+            </div>
           </div>
-          <div className="loginbtn">
-            <UserLogState className="fonts" className="headerbutton" />
-          </div>
+          
+        </header>
+        <div>
+          <Movies movies={movies} />
+          <Movies movies={movies} />
         </div>
-      </header>
+      </>
     );
 }
