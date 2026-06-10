d221899f2aac2c7ed5f004299aab31a884420c07
KikhGr <kyriakh.d.grigoriadi@gmail.com>
2026-06-10 23:42:47 +0300

    Update Theme.jsx

--- HCI Group Assignment/src/components/Theme.jsx
+++ HCI Group Assignment/src/components/Theme.jsx
@@ -9,8 +9,8 @@ export default function changeTheme () {
 
     return (
         <button className="hbuttonin" onClick={handleChange}>
-      <img src={`src/assets/${theme}`} />
-    </button>
+          <img src={`src/assets/${theme}`} />
+        </button>
     );
     
 }
